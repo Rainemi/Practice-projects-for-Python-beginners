76ed1c148089c81982c2a8cdf4aa6d272a51ff1c
Rainemi <105657663+Rainemi@users.noreply.github.com>
2022-09-11 09:03:48 +0100

    Add files via upload

---
+++ leap_year.py
@@ -0,0 +1,20 @@
+
+
+
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap Year")
+            else:
+                print("Not leap Year")
+        else:
+            print("Leap Year")
+    else:
+        print("Not Leap Year")
+
+is_leap_year(2039)
+
+        
+
+            
