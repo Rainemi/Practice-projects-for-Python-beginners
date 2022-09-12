5e30352f76f45e3bca20e7e78de22822715ad80b
Rainemi <105657663+Rainemi@users.noreply.github.com>
2022-09-12 12:40:01 +0100

    Update word_dictionary.py

--- word_dictionary.py
+++ word_dictionary.py
@@ -1,18 +1,15 @@
 
 
-def main():
-    word_dictionary = {
-    "hi" : "a word for greeting",
-    "earth": "the third planet in the solar system",
-    "milk" : "a substance gotten from animals udder"
-    }
+from PyDictionary import PyDictionary
 
-    while True:
-        word = input("Enter a word : ")
-        if word == "":
-            break
-        if word in word_dictionary:
-            print(word, ":", word_dictionary(word))
-print(main())
+dictionary = PyDictionary()
+
+while True:
+    word = input("Enter a word : ")
+    print(dictionary.meaning(word))
+    if word == "":
+        break
+    
+    
 
 #still under review not yet complete
